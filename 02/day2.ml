4b1ae11ecc2f8159d0432aa4f6591500857652ba
jdl <confusionary@gmail.com>
2019-12-02 21:51:51 -0500

    Day 2 Part 2 complete

--- 02/day2.ml
+++ 02/day2.ml
@@ -3,7 +3,7 @@ open Core
 (** part 1 answer 12490719 **)
 
 let input = In_channel.read_all "02/input.txt"
-let opcodes = Array.map (Array.of_list (String.split ~on:',' input)) ~f:Int.of_string
+let get_opcodes = Array.map (Array.of_list (String.split ~on:',' input)) ~f:Int.of_string
 
 (* returns the next pc *)
 let execute_instruction pc data =
@@ -20,7 +20,7 @@ let execute_instruction pc data =
     let () = Array.set data loc (a * b) in
     pc + 4
   | 99 -> -1
-  | _ -> raise (Invalid_argument "Invalid opcode")
+  | o -> raise (Invalid_argument ("Invalid opcode " ^ (Int.to_string o)))
 
 let rec execute pc data =
   let new_pc = execute_instruction pc data in
@@ -28,5 +28,24 @@ let rec execute pc data =
   | -1 -> Array.get data 0
   | x -> execute x data
 
-let v = execute 0 opcodes 
-let () = print_endline (Int.to_string v)
+let run (noun, verb) =
+  let o = Array.map (Array.of_list (String.split ~on:',' input)) ~f:Int.of_string in
+  (* let () = print_endline (List.to_string ~f:Int.to_string (Array.to_list o)) in *)
+  let () = Array.set o 1 noun in
+  let () = Array.set o 2 verb in
+  (* let () = print_endline (List.to_string ~f:Int.to_string (Array.to_list o)) in *)
+  let result = execute 0 o in
+  (* let () = print_endline (List.to_string ~f:Int.to_string (Array.to_list o)) in *)
+  let () = print_endline (Int.to_string result ^ " " ^ Int.to_string noun ^ " " ^ Int.to_string verb) in
+  match result with
+  | 19690720 -> (noun, verb)
+  | _ -> (-1, -1)
+
+let nouns = List.range 0 100
+let verbs = List.range 0 100
+
+let candidates = List.cartesian_product nouns verbs
+
+let results = List.map candidates ~f:run
+let pairs = List.filter results ~f:(fun (x,y) -> x != -1 && y != -1)
+let () = List.iter pairs ~f:(fun (n,v) -> print_endline ((Int.to_string n) ^ (Int.to_string v)))
