82b1c8505d1dbd64c1dafae1951b20c4cf6d9b26
jdl <confusionary@gmail.com>
2019-12-04 21:27:25 -0500

    Day 3 Part 2 complete

--- 03/day3.ml
+++ 03/day3.ml
@@ -1,16 +1,16 @@
 open Core
 
-(* let input = In_channel.read_lines "03/input.txt" *)
-(* let paths = List.map ~f:(String.split ~on:',') input *)
-(* let path1 = List.nth_exn paths 0 *)
-(* let path2 = List.nth_exn paths 1 *)
+let input = In_channel.read_lines "03/input.txt"
+let paths = List.map ~f:(String.split ~on:',') input
+let path1 = List.nth_exn paths 0
+let path2 = List.nth_exn paths 1
 
 
 (* let path1 = ["R8";"U5";"L5";"D3"] *)
-let path1 = ["R75";"D30";"R83";"U83";"L12";"D49";"R71";"U7";"L72"]
+(* let path1 = ["R75";"D30";"R83";"U83";"L12";"D49";"R71";"U7";"L72"] *)
 
 (* let path2 = ["U7";"R6";"D4";"L4"] *)
-let path2 = ["U62";"R66";"U55";"R34";"D71";"R55";"D58";"R83"]
+(* let path2 = ["U62";"R66";"U55";"R34";"D71";"R55";"D58";"R83"] *)
 
 let direction path = String.prefix path 1
 let distance path dir = String.chop_prefix_exn path ~prefix:dir |> Int.of_string
@@ -23,10 +23,10 @@ let string_to_point s = let splits = String.split ~on:':' s in
 
 let move (x,y) direction distance =
   match direction with 
-  | "L" -> List.map (List.range ~stride:(-1) (x - 1) (x - 1 - distance)) ~f:(fun x2 -> point_to_string (x2, y))
+  | "L" -> List.map (List.range ~stride:(-1) (x) (x - distance - 1)) ~f:(fun x2 -> point_to_string (x2, y))
   | "R" -> List.map (List.range ~stride:(1) x (x + distance + 1)) ~f:(fun x2 -> point_to_string (x2, y))
   | "U" -> List.map (List.range ~stride:(1) y (y + distance + 1)) ~f:(fun y2 -> point_to_string (x, y2))
-  | "D" -> List.map (List.range ~stride:(-1) (y - 1) (y - 1 - distance)) ~f:(fun y2 -> point_to_string (x, y2))
+  | "D" -> List.map (List.range ~stride:(-1) (y) (y - distance - 1)) ~f:(fun y2 -> point_to_string (x, y2))
   | _ -> raise (Invalid_argument("unknown direction " ^ direction))
 
 let hash = Hashtbl.create (module String)
@@ -44,24 +44,26 @@ let p1 = List.fold path1 ~init:("1:1", 0)
         let dir = direction a in
         let dist = distance a dir in
         let points = move (string_to_point point) dir dist in
-        let () = List.iteri points ~f:(fun i key -> update_hash hash key (total + i + 1)) in
+        let () = List.iteri points ~f:(fun i key -> update_hash hash key (total + i)) in
         (List.last_exn points, total + dist)
     )
 
-(* let () = Hashtbl.iteri hash ~f:(
-    fun ~key ~data -> print_endline (key ^ " " ^ Int.to_string data)
-   ) *)
+(* let () = Hashtbl.iteri hash 
+    ~f:(
+      fun ~key ~data -> print_endline (key ^ " " ^ Int.to_string data)
+    ) *)
 
 let intersections = Hashtbl.create (module String)
 
-let print_intersections h = Hashtbl.iteri h ~f:(
-    fun ~key:point ~data:freq -> print_endline (point ^ " " ^ Int.to_string freq)
-  )
+let print_intersections h = Hashtbl.iteri h 
+    ~f:(
+      fun ~key:point ~data:freq -> print_endline (point ^ " " ^ Int.to_string freq)
+    )
 
 let update_intersections h point v =
   let freq = Hashtbl.find h point in
   match freq with
-  | Some f -> ()
+  | Some _ -> ()
   | None -> Hashtbl.set h ~key:point ~data:v
 
 let p2 = List.fold path2 ~init:("1:1", 0)
@@ -74,10 +76,11 @@ let p2 = List.fold path2 ~init:("1:1", 0)
             ~f:(
               fun i point -> let p = Hashtbl.find hash point in
                 match p with
-                | Some f -> update_intersections intersections point (total + i + 1 + f)
+                | Some f -> update_intersections intersections point (total + i + f)
                 | None -> ()
             ) in
         (List.last_exn points, total + dist)
     )
 
+let () = print_endline ""
 let () = print_intersections intersections
