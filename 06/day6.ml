53c8c564aac0228c8b783e70a80f27268a02c0b0
jdl <confusionary@gmail.com>
2019-12-07 00:54:16 -0500

    Day 6 Part 2 complete

--- 06/day6.ml
+++ 06/day6.ml
@@ -6,16 +6,22 @@ let setup pair o = let s = String.split pair ~on:')' in
   match s with
   | [orbitee;orbiter] -> let () = Hashtbl.set orbits ~key:orbiter ~data:orbitee in
     let s = Set.add o orbiter in
-    let () = print_endline @@ Printf.sprintf "Adding %s ) %s" orbiter orbitee in
     Set.add s orbitee
   | _ -> failwith "Invalid orbit"
 
-
-let rec traverse count start =
+let rec path start = 
   let next = Hashtbl.find orbits start in
   match next with
-  | Some next -> let () = print_endline (Printf.sprintf "Going from %s to %s" start next) in 
-    traverse (count + 1) next
+  | Some next -> 
+    List.append [start] (path next)
+  | None -> []
+
+let rec traverse count start stop =
+  let next = Hashtbl.find orbits start in
+  match next with
+  | Some next -> 
+    if (phys_equal next stop) then count else
+      traverse (count + 1) next stop
   | None -> count
 
 let input = In_channel.read_lines "06/input.txt" 
@@ -25,9 +31,27 @@ let objects = List.fold input ~init:(Set.empty (module String))
       fun acc a -> setup a acc
     )
 
-let () = print_endline @@ "objects " ^ (Int.to_string (Set.length objects))
-let () = print_endline @@ "orbits " ^ (Int.to_string (Hashtbl.length orbits))
+let t count stop = fun s -> traverse count s stop
 
-let count = List.map (Set.to_list objects) ~f:(traverse 0) |> List.sum (module Int) ~f:ident
+let count = List.map (Set.to_list objects) ~f:(t 0 "") |> List.sum (module Int) ~f:ident
 
-let () = print_endline (Printf.sprintf "%d" count)
+let san = Hashtbl.find_exn orbits "SAN"
+let me = Hashtbl.find_exn orbits "YOU"
+
+let santa_path = path san
+let my_path = path me
+
+let common = List.fold_until ~init:"" my_path 
+    ~finish:(fun _ -> "")
+    ~f:(
+      fun _ a -> 
+        let t = List.find santa_path ~f:(fun x -> phys_equal x a) in
+        match t with
+        | Some o -> Stop(o)
+        | None -> Continue("")
+    )
+
+let c1 = traverse 0 me common
+let c2 = traverse 0 san common
+
+let () = print_endline (Int.to_string (c1 + c2))
