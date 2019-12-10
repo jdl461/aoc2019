8f1b61c8d6edc178970052a186e95195cdcf4873
jdl <confusionary@gmail.com>
2019-12-10 08:29:56 -0500

    Day 8 Part 2 complete

--- 08/day8.ml
+++ 08/day8.ml
@@ -7,9 +7,9 @@ let data = In_channel.read_all "08/input.txt"
 
 let count_layer_digits layer digit = List.count layer ~f:(fun d -> (phys_equal d digit ))
 
-let part1 () =
-  let layers = List.chunks_of (String.to_list data) ~length:(width * height) in
+let layers = List.chunks_of (String.to_list data) ~length:(width * height)
 
+let part1 () =
   let counts = List.mapi layers ~f:(fun idx layer -> (idx, count_layer_digits layer '0')) in
 
   let min = List.min_elt counts ~compare:(fun (_,count1) (_, count2) -> Int.compare count1 count2) in
@@ -24,4 +24,25 @@ let part1 () =
   let answer = ones * twos in
   print_endline (Printf.sprintf "%d" answer)
 
+
+let part2 () = 
+  let overlay oldpixel newpixel = 
+    if (phys_equal oldpixel '2')
+    then newpixel
+    else oldpixel in
+
+  let overlay_frame oldframe newframe =
+    List.map2_exn oldframe newframe ~f:overlay in
+
+  let image = List.fold (List.drop layers 1) ~init:(List.nth_exn layers 0) 
+      ~f:(fun acc a -> overlay_frame acc a) in
+
+  let rows = List.chunks_of image ~length:width in
+
+  List.iter rows 
+    ~f:(
+      fun row -> let r = String.tr (String.of_char_list row) ~target:'0' ~replacement:' ' in print_endline r
+    )
+
 let () = part1 ()
+let () = part2 ()
