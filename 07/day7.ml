ff40be643277bfa59e33a30c0b523421f07d0a1f
jdl <confusionary@gmail.com>
2019-12-09 23:19:54 -0500

    Day 7 Part 2 complete

--- 07/day7.ml
+++ 07/day7.ml
@@ -1,26 +1,99 @@
 open Core
 open Permutations
 
+
+let list_to_string lst = List.to_string ~f:ident lst
+
 let data = In_channel.read_all "07/input.txt"
-let get_instructions = Array.append (Array.of_list (String.split ~on:',' data)) (Array.create ~len:5000 "0")
+(* let data = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5" *)
 
-let rec execute pc data input_stack output_stack =
-  let (new_pc, data, new_input_stack, new_output_stack, exit_code) = Intcode.execute_instruction pc data input_stack output_stack in
-  match new_pc with
-  | -1 -> new_output_stack
-  | x -> execute x data new_input_stack new_output_stack
+let get_instructions () = Array.append (Array.of_list (String.split ~on:',' data)) (Array.create ~len:0 "0")
 
 
-let run_sequence phases =
-  List.fold phases ~init:"0" 
-    ~f:(
-      fun acc a -> let output = execute 0 get_instructions [a; acc] [] in
-        List.last_exn output
-    )
+let rec execute (amp_state : Ampstate.amp_state) =
+  let new_amp_state = 
+    Intcode.execute_instruction amp_state in
 
-let part1 = let phases = Permutations.permutations ["0";"1";"2";"3";"4"] in
+  match new_amp_state.exit_code with
+  | 0 ->
+    (  match new_amp_state.pc with
+       | -1 -> new_amp_state
+       | x -> execute { 
+           new_amp_state with 
+           pc = x; 
+           input_stack = new_amp_state.input_stack; 
+           output_stack = new_amp_state.output_stack
+         }
+    )  
+  | _ -> new_amp_state
+
+
+let part1 () = 
+  let run_sequence phases =
+    List.fold phases ~init:"0" 
+      ~f:(
+        fun acc a -> 
+          let new_amp_state = 
+            execute {
+              pc = 0; 
+              data = (get_instructions ()); 
+              input_stack = [a; acc]; 
+              output_stack = []; 
+              exit_code = 0
+            } in
+          List.last_exn new_amp_state.output_stack
+      ) in
+  let phases = Permutations.permutations ["0";"1";"2";"3";"4"] in
   let signals = List.map phases ~f:run_sequence in
   let output = List.max_elt signals ~compare:(fun a b -> Int.compare (Int.of_string a) (Int.of_string b)) in
   match output with
   | Some signal -> print_endline signal
   | _ -> failwith "No signal"
+
+let part2 () = 
+  let setup_amp phase signal = execute {
+      pc = 0;
+      data = get_instructions (); 
+      input_stack = [phase; signal]; 
+      output_stack = []; 
+      exit_code = 0
+    } in
+
+
+  let setup_phases phases = 
+    match phases with
+    | a :: b :: c :: d :: e :: [] ->
+      let a1 = setup_amp a "0" in
+      let a2 = setup_amp b (List.last_exn a1.output_stack) in
+      let a3 = setup_amp c (List.last_exn a2.output_stack) in
+      let a4 = setup_amp d (List.last_exn a3.output_stack) in
+      let a5 = setup_amp e (List.last_exn a4.output_stack) in
+      [a1;a2;a3;a4;a5]
+    | _ -> failwith "Error" in
+
+
+  let rec run_phase amps = match amps with
+    | [amp1;amp2;amp3;amp4;(amp5 : Ampstate.amp_state)] ->
+      if amp1.exit_code = -1 then [amp1;amp2;amp3;amp4;amp5]
+      else 
+        let amp1 = execute { amp1 with input_stack = [List.last_exn amp5.output_stack]} in
+        let amp2 = execute { amp2 with input_stack = [List.last_exn amp1.output_stack]} in
+        let amp3 = execute { amp3 with input_stack = [List.last_exn amp2.output_stack]} in
+        let amp4 = execute { amp4 with input_stack = [List.last_exn amp3.output_stack]} in
+        let amp5 = execute { amp5 with input_stack = [List.last_exn amp4.output_stack]} in
+        run_phase [amp1;amp2;amp3;amp4;amp5]
+    | _ -> failwith "Error" in
+
+  let p = Permutations.permutations ["9";"8";"7";"6";"5"] in
+  let m = List.map p ~f:setup_phases in
+  let x = List.map m ~f:run_phase in
+  let a = List.map x ~f:(fun x -> let a = List.last_exn x in Int.of_string (List.last_exn a.output_stack)) in
+
+  let max = List.max_elt a ~compare:Int.compare  in
+
+  match max with 
+  | Some m -> print_endline (Int.to_string m)
+  | None -> print_endline "Error"
+
+let () = part1 ()
+let () = part2 ()
--- 07/intcode.ml
+++ 07/intcode.ml
@@ -42,64 +42,74 @@ let set_param (data : string array) (offset : int) (param : string) =
   data.(offset) <- param
 
 
-let instruction_3 pc data input_stack output_stack = 
-  let input = List.hd input_stack in
+let instruction_3 (amp_state : Ampstate.amp_state) : Ampstate.amp_state = 
+  let input = List.hd amp_state.input_stack in
   match input with
   | Some i ->
-    let input_stack = List.drop input_stack 1 in
-    let loc = Int.of_string (get_param data (pc + 1) "1") in
-    let () = set_param data loc i in
-    (pc + 2, data, input_stack, output_stack, 0)
-  | None -> (pc, data, input_stack, output_stack, 1)
+    (* let () = print_endline ("Got input " ^ i) in *)
+    let input_stack = List.drop amp_state.input_stack 1 in
+    let loc = Int.of_string (get_param amp_state.data (amp_state.pc + 1) "1") in
+    let () = set_param amp_state.data loc i in
+    let new_pc = amp_state.pc + 2 in
+    { amp_state with 
+      pc = new_pc; 
+      input_stack = input_stack;
+      exit_code = 0; 
+    } 
+  | None -> 
+    (* let () = print_endline ("No input, pausing") in *)
+    { amp_state with exit_code = 1 }
 
 
-(* returns the next pc *)
-let execute_instruction pc data input_stack output_stack =
-  let instruction =  data.(pc) in
+let execute_instruction (amp_state : Ampstate.amp_state) : Ampstate.amp_state =
+  let instruction =  amp_state.data.(amp_state.pc) in
   let opcode = parse_opcode instruction in
   let (_,m2,m1) = parse_modes instruction in
   match opcode with
   | "01" -> 
-    let loc = Int.of_string (get_param data (pc + 3) "1") in
-    let a = Int.of_string (get_param data (pc + 1) m1) in
-    let b = Int.of_string (get_param data (pc + 2) m2) in
-    let () = set_param data loc (Int.to_string (a + b)) in
-    (pc + 4, data, input_stack, output_stack, 0)
+    let loc = Int.of_string (get_param amp_state.data (amp_state.pc + 3) "1") in
+    let a = Int.of_string (get_param amp_state.data (amp_state.pc + 1) m1) in
+    let b = Int.of_string (get_param amp_state.data (amp_state.pc + 2) m2) in
+    let () = set_param amp_state.data loc (Int.to_string (a + b)) in
+    { amp_state with pc = amp_state.pc + 4; exit_code = 0 }
   | "02" -> 
-    let loc = Int.of_string (get_param data (pc + 3) "1") in
-    let a = Int.of_string (get_param data (pc + 1) m1) in
-    let b = Int.of_string (get_param data (pc + 2) m2) in
-    let () = set_param data loc (Int.to_string (a * b)) in
-    (pc + 4, data,input_stack, output_stack, 0)
-  | "03" -> instruction_3 pc data input_stack output_stack
+    let loc = Int.of_string (get_param amp_state.data (amp_state.pc + 3) "1") in
+    let a = Int.of_string (get_param amp_state.data (amp_state.pc + 1) m1) in
+    let b = Int.of_string (get_param amp_state.data (amp_state.pc + 2) m2) in
+    let () = set_param amp_state.data loc (Int.to_string (a * b)) in
+    {amp_state with pc = amp_state.pc + 4; exit_code = 0 }
+  | "03" -> instruction_3 amp_state
   | "04" -> 
-    let a = get_param data (pc + 1) m1 in
-    let output_stack = List.append output_stack [a] in
-    (pc + 2, data, input_stack, output_stack, 0)
+    let a = get_param amp_state.data (amp_state.pc + 1) m1 in
+    (* let () = print_endline ("Output: " ^ a) in *)
+    let output_stack = List.append amp_state.output_stack [a] in
+    { amp_state with pc = amp_state.pc + 2 ; output_stack = output_stack ; exit_code = 0}
   | "05" -> 
-    let a = Int.of_string (get_param data (pc + 1) m1) in
-    let b = Int.of_string (get_param data (pc + 2) m2) in
-    if (not @@ phys_equal a 0) then (b, data, input_stack, output_stack, 0) 
-    else (pc + 3, data, input_stack, output_stack, 0)
+    let a = Int.of_string (get_param amp_state.data (amp_state.pc + 1) m1) in
+    let b = Int.of_string (get_param amp_state.data (amp_state.pc + 2) m2) in
+    if (not @@ phys_equal a 0) 
+    then { amp_state with pc = b ; exit_code = 0 }
+    else { amp_state with pc = amp_state.pc + 3; exit_code = 0}
   | "06" ->
-    let a = Int.of_string (get_param data (pc + 1) m1) in
-    let b = Int.of_string (get_param data (pc + 2) m2) in
-    if (phys_equal a 0) then (b, data, input_stack, output_stack, 0) 
-    else (pc + 3, data, input_stack, output_stack, 0)
+    let a = Int.of_string (get_param amp_state.data (amp_state.pc + 1) m1) in
+    let b = Int.of_string (get_param amp_state.data (amp_state.pc + 2) m2) in
+    if (phys_equal a 0) 
+    then { amp_state with pc = b; exit_code = 0}
+    else { amp_state with pc = amp_state.pc + 3; exit_code = 0}
   | "07" -> 
-    let loc = Int.of_string (get_param data (pc + 3) "1") in
-    let a = Int.of_string (get_param data (pc + 1) m1) in
-    let b = Int.of_string (get_param data (pc + 2) m2) in
+    let loc = Int.of_string (get_param amp_state.data (amp_state.pc + 3) "1") in
+    let a = Int.of_string (get_param amp_state.data (amp_state.pc + 1) m1) in
+    let b = Int.of_string (get_param amp_state.data (amp_state.pc + 2) m2) in
     let v = if a < b then "1" else "0" in
-    let () = set_param data loc v in
-    (pc + 4, data, input_stack, output_stack, 0)
+    let () = set_param amp_state.data loc v in
+    { amp_state with pc = amp_state.pc + 4; exit_code = 0 }
   | "08" ->
-    let loc = Int.of_string (get_param data (pc + 3) "1") in
-    let a = Int.of_string (get_param data (pc + 1) m1) in
-    let b = Int.of_string (get_param data (pc + 2) m2) in
+    let loc = Int.of_string (get_param amp_state.data (amp_state.pc + 3) "1") in
+    let a = Int.of_string (get_param amp_state.data (amp_state.pc + 1) m1) in
+    let b = Int.of_string (get_param amp_state.data (amp_state.pc + 2) m2) in
     let v = if (phys_equal a b) then "1" else "0" in
-    let () = set_param data loc v in
-    (pc + 4, data, input_stack, output_stack, 0)
+    let () = set_param amp_state.data loc v in
+    { amp_state with pc = amp_state.pc + 4; exit_code = 0}
   | "99" -> 
-    (-1, data, input_stack, output_stack, -1)
+    { amp_state with pc = -1; exit_code = -1}
   | _ -> raise (Invalid_argument ("Invalid instruction " ^ opcode))
