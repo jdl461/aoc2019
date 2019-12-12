073b334b5eb458e9fe1b5322cfce8d969675f860
jdl <confusionary@gmail.com>
2019-12-11 22:09:16 -0500

    Day 9 Part 2 complete

--- 09/day9.ml
+++ 09/day9.ml
@@ -7,16 +7,23 @@ let get_instructions () = let h = Hashtbl.create (module Bigint) in
   let () = List.iteri instructions ~f:(fun idx instr -> Hashtbl.set h ~key:(Bigint.of_int idx) ~data:instr) in
   h
 
-let day1 () = 
-  let r = Intcode.execute {
-      pc = Bigint.of_int 0;
-      data = get_instructions ();
-      input_stack = ["1"];
-      output_stack = [];
-      exit_code = 0;
-      relative_base = 0;
-    } in
+let run_with_input input = 
+  Intcode.execute {
+    pc = Bigint.of_int 0;
+    data = get_instructions ();
+    input_stack = [input];
+    output_stack = [];
+    exit_code = 0;
+    relative_base = 0;
+  }
 
+let day1 () = 
+  let r = run_with_input "1" in
+  print_endline (Ampstate.list_to_string r.output_stack)
+
+let day2 () =
+  let r = run_with_input "2" in
   print_endline (Ampstate.list_to_string r.output_stack)
 
 let () = day1 ()
+let () = day2 ()
