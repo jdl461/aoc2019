8530aa6c1224b33734c758e0b31a5d536d4a0e1d
jdl <confusionary@gmail.com>
2019-12-16 23:03:11 -0500

    Day 12 Part 1 in progress

---
+++ 12/day12.ml
@@ -0,0 +1,75 @@
+open Core
+
+(* let io = (10,15,7)
+   let europa = (15,10,0)
+   let ganymede = (20,12,3)
+   let callisto = (0,3,13) *)
+
+
+let ma = (-1, 0, 2)
+let mb = (2, -10, -7)
+let mc = (4, -8, 8)
+let md = (3, 5, -1)
+
+let moon_to_string (x1,y1,z1) = (Printf.sprintf "<%d,%d,%d>" x1 y1 z1)
+let print_moon (m,v) = print_endline @@ (moon_to_string m) ^ " " ^ (moon_to_string v)
+
+let compare (x1,y1,z1) (x2,y2,z2) = 
+  (
+    (Int.compare x1 x2) * (-1),
+    (Int.compare y1 y2) * (-1),
+    (Int.compare z1 z2) * (-1)
+  )
+
+let update (x1,y1,z1) (x2,y2,z2) = (x1 + x2, y1 + y2, z1 + z2)
+
+let calc_velocity moon_list cur_vel = 
+  let mvs = List.map moon_list ~f:(fun (m1,m2) -> compare m1 m2) in
+  List.fold mvs ~init:cur_vel ~f:update
+
+let add_abs (x,y,z) = (abs x) + (abs y) + (abs z)
+let potential_energy pos = add_abs pos
+let kinetic_energy vel = add_abs vel
+
+let pairs m1 m2 m3 m4 =
+  [
+    (m1,m2);
+    (m1,m3);
+    (m1,m4)
+  ]
+
+let step (m1, v1) (m2, v2) (m3, v3) (m4, v4) = 
+  let m1s = pairs m1 m2 m3 m4 in
+  let m1v = calc_velocity m1s v1 in
+  let new_m1 = update m1 m1v in
+
+  let m2s = pairs m2 m1 m3 m4 in
+  let m2v = calc_velocity m2s v2 in
+  let new_m2 = update m2 m2v in
+
+  let m3s = pairs m3 m1 m2 m4 in
+  let m3v = calc_velocity m3s v3 in
+  let new_m3 = update m3 m3v in
+
+  let m4s = pairs m4 m2 m3 m1 in
+  let m4v = calc_velocity m4s v4 in
+  let new_m4 = update m4 m4v in
+
+  [(new_m1, m1v); (new_m2, m2v); (new_m3, m3v); (new_m4, m4v)]
+
+
+let print_step step = 
+  match step with
+  | [a;b;c;d] -> 
+    List.iter [a;b;c;d] ~f:(fun m -> (print_moon m))
+  | _ -> failwith "bad output"
+
+let zero_v = (0,0,0)
+
+let step1 = step (ma, zero_v) (mb, zero_v) (mc, zero_v) (md, zero_v)
+
+let step2 = match step1 with
+  | [(p1,v1);(p2,v2);(p3,v3);(p4,v4);] -> step (p1,v1) (p2, v2) (p3, v3) (p4, v4)
+  | _ -> failwith "invalid step"
+
+let () = print_step step2
